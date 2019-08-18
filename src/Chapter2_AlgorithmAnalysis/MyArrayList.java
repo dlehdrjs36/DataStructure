df15c4692f482c7eed1cf27b23b3dcd94e2ef615
이동건 <slsldkf123@naver.com>
2019-08-18 20:39:52 +0900

    20190818 배열리스트 테스트

---
+++ src/Chapter2_AlgorithmAnalysis/MyArrayList.java
@@ -0,0 +1,266 @@
+package Chapter2_AlgorithmAnalysis;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * @author downey
+ * @param <T>
+ *
+ */
+public class MyArrayList<T> implements List<T> {
+	int size;                    // keeps track of the number of elements
+	private T[] array;           // stores the elements
+
+	/**
+	 *
+	 */
+	@SuppressWarnings("unchecked")
+	public MyArrayList() {
+		// You can't instantiate an array of T[], but you can instantiate an
+		// array of Object and then typecast it.  Details at
+		// http://www.ibm.com/developerworks/java/library/j-jtp01255/index.html
+		array = (T[]) new Object[10];
+		size = 0;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// run a few simple tests
+		MyArrayList<Integer> mal = new MyArrayList<Integer>();
+		mal.add(1);
+		mal.add(2);
+		mal.add(3);
+		System.out.println(Arrays.toString(mal.toArray()) + " size = " + mal.size);
+
+		mal.remove(new Integer(2));
+		System.out.println(Arrays.toString(mal.toArray()) + " size = " + mal.size);
+	}
+
+	@Override
+	public boolean add(T element) {
+		// TODO: FILL THIS IN!
+		if (size >= array.length) {
+			T[] bigger = (T[]) new Object[array.length * 2];
+			System.arraycopy(array, 0, bigger, 0, array.length);
+			array = bigger;
+		}
+		array[size] = element;
+		size++;
+		return true;
+	}
+
+	@Override
+	public void add(int index, T element) {
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException();
+		}
+		// add the element to get the resizing
+		add(element);
+
+		// shift the elements
+		for (int i=size-1; i>index; i--) {
+			array[i] = array[i-1];
+		}
+		// put the new one in the right place
+		array[index] = element;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> collection) {
+		boolean flag = true;
+		for (T element: collection) {
+			flag &= add(element);
+		}
+		return flag;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends T> collection) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		// note: this version does not actually null out the references
+		// in the array, so it might delay garbage collection.
+		size = 0;
+	}
+
+	@Override
+	public boolean contains(Object obj) {
+		return indexOf(obj) != -1;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> collection) {
+		for (Object element: collection) {
+			if (!contains(element)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public T get(int index) {
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		return array[index];
+	}
+
+	//해당하는 값을 가지는 인덱스 번호를 검사, 추가한부분
+	@Override
+	public int indexOf(Object target) {
+		// TODO: FILL THIS IN!
+		for(int idx=0; idx<size; idx++) {
+			// 조건을 equals(target, array[i])로 변경하기
+			if ( target == null? get(idx) == null : target.equals(get(idx))) {		
+				return idx;
+			}
+		}
+		//못 찾은경우
+		return -1;
+	}
+
+	/** Checks whether an element of the array is the target.
+	 *
+	 * Handles the special case that the target is null.
+	 *
+	 * @param target
+	 * @param object
+	 */
+	private boolean equals(Object target, Object element) {
+		if (target == null) {
+			return element == null;
+		} else {
+			return target.equals(element);
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// make a copy of the array
+		T[] copy = Arrays.copyOf(array, size);
+		// make a list and return an iterator
+		return Arrays.asList(copy).iterator();
+	}
+
+	@Override
+	public int lastIndexOf(Object target) {
+		// see notes on indexOf
+		for (int i = size-1; i>=0; i--) {
+			if (equals(target, array[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	@Override
+	public ListIterator<T> listIterator() {
+		// make a copy of the array
+		T[] copy = Arrays.copyOf(array, size);
+		// make a list and return an iterator
+		return Arrays.asList(copy).listIterator();
+	}
+
+	@Override
+	public ListIterator<T> listIterator(int index) {
+		// make a copy of the array
+		T[] copy = Arrays.copyOf(array, size);
+		// make a list and return an iterator
+		return Arrays.asList(copy).listIterator(index);
+	}
+
+	@Override
+	public boolean remove(Object obj) {
+		int index = indexOf(obj);
+		if (index == -1) {
+			return false;
+		}
+		remove(index);
+		return true;
+	}
+//추가한 부분
+	@Override
+	public T remove(int index) {
+		// TODO: FILL THIS IN!
+		if ( index <0 || index >= size ) {
+			throw new IndexOutOfBoundsException();
+		}
+		// array[index]는 get(index)로 할 수도 있음
+		T result = array[index];
+		
+		//삭제되는 요소의 뒤의 값을 한 칸씩 당긴다. size는 size-1로 수정하기
+		for(int idx=index; idx<size-1; idx++) {
+			array[idx] = array[idx+1];
+		}
+		size--;
+		return result;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> collection) {
+		boolean flag = true;
+		for (Object obj: collection) {
+			flag &= remove(obj);
+		}
+		return flag;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> collection) {
+		throw new UnsupportedOperationException();
+	}
+
+	//추가한 부분
+	@Override
+	public T set(int index, T element) {
+		
+		if (  index < 0 || index > size ) {
+			throw new IndexOutOfBoundsException();
+		}
+		//이전 인덱스의 값, array[index]는 get(index)로도 할 수 있음
+		T result = array[index];
+		array[index] = element;
+		
+		return result;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@Override
+	public List<T> subList(int fromIndex, int toIndex) {
+		if (fromIndex < 0 || toIndex >= size || fromIndex > toIndex) {
+			throw new IndexOutOfBoundsException();
+		}
+		T[] copy = Arrays.copyOfRange(array, fromIndex, toIndex);
+		return Arrays.asList(copy);
+	}
+
+	@Override
+	public Object[] toArray() {
+		return Arrays.copyOf(array, size);
+	}
+
+	@Override
+	public <U> U[] toArray(U[] array) {
+		throw new UnsupportedOperationException();
+	}
+	
+}
--- src/Chapter2_AlgorithmAnalysis/SelectionSort.java
+++ src/Chapter2_AlgorithmAnalysis/SelectionSort.java
@@ -6,6 +6,14 @@ package Chapter2_AlgorithmAnalysis;
 import java.util.Arrays;
 
 /**
+ * 선택정렬
+ * 모든 요소를 검사하여 가장 작은 수 혹은 큰 수대로 정렬한다
+ * 특정 요소를 선택하여 나머지 요소와 비교를하여 정렬하기 때문에 선택정렬이라고 한다.
+ * 1. 특정 요소 선택
+ * 2. 선택한 요소 이외의 값들과 선택한 요소 비교
+ * 3. 선택한 요소의 값보다 작은 수가 존재하면, 요소의 위치변경(Swap)
+ * 4. 1~3 반복
+ * 
  * <p><strong>selectionSort() 내부 for문 비교 횟수</strong></p>                       
  * 0, 1 , ... , n-1, n                                                   
  * <p><strong>indexLowest() 비교 횟수</strong></p>                                 
@@ -38,7 +46,10 @@ public class SelectionSort {
 	 * Finds the index of the lowest value
 	 * between indices low and high (inclusive).
 	 * start로부터 시작하는 최솟값의 위치를 찾고(start 포함) 배열의 마지막 위치로 이동한다.
-	 * 선형 (n-start) O(n)
+	 * 배열의 크기 : n
+	 * 배열비교시작점 : start
+	 * n-start만큼 비교 수행
+	 * 선형 O(n)
 	 */
 	public static int indexLowest(int[] array, int start) {
 		int lowIndex = start;
